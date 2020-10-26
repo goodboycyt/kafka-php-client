84a478197835358a10cc6804d0d7fbb1dae7a4b2
yatongchen <1836096205@qq.com>
2020-10-26 17:11:36 +0800

    socket 客户端

---
+++ src/KafkaSkProducer.php
@@ -0,0 +1,82 @@
+<?php
+namespace KafkaPhp;
+
+require 'KafkaException.php';
+
+/**
+ * Class KafkaSkProducer
+ *
+ * special use this to send msg to pass system by socket,then pass system transmit msg to Kafka Services.
+ * Users could do operations like producing messages via an KafkaSkProducer instance.
+ * For more details, please check out the Kafka API document:https://github.com/goodboycyt/kafka-php-client
+ * @package Kafka
+ * @version 1.0
+ * @author linwanfeng
+ */
+class KafkaSkProducer
+{
+    /**
+     * Constructor
+     *
+     * Nothing to say.
+     *
+     * @param string $host The host of Kafka server with port like 193.23.1.1:9093
+     * @throws KafkaException
+     */
+    public function __construct($host, $port)
+    {
+        $host = trim($host);
+        if (empty($host)) {
+            throw new KafkaException(['code'=>30,'message'=>'host is empty']);
+        }
+        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, array("sec" => 1, "usec" => 10000));
+        socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, array("sec" => 1, "usec" => 10000));
+        if (socket_connect($this->socket, $host, $port) == false) {
+            socket_close($this->socket);//工作完毕，关闭套接流
+            throw new KafkaException(['code'=>37,'message'=>'host connect fail']);
+        }
+    }
+
+    /**
+     * Producer a message
+     *
+     * Nothing to say.
+     *
+     * @param string $msg msg body
+     * @param string $topic which topic to push msg.
+     * @throws KafkaException
+     */
+    public function sendMsg($topic, $msg)
+    {
+        if (empty($msg) || empty($topic) || $this->socket==null) {
+            throw new KafkaException(['code'=>55,'message'=>'topic or msg or socket is empty']);
+        }
+
+            $data['topic'] = $topic;
+            $data['msg'] = $msg;
+            $message = json_encode($data);
+            $message = mb_convert_encoding($message, 'GBK', 'UTF-8');
+        try {
+            if (socket_write($this->socket, $message, strlen($message)) == false) {
+                socket_close($this->socket);//工作完毕，关闭套接流
+                throw new KafkaException(['code'=>63,'message'=>'socket 写入错误']);
+            } else {
+                while ($callback = socket_read($this->socket, 2048)) {
+                    echo $callback;
+                    break;
+                }
+            }
+//            socket_close($this->socket);//工作完毕，关闭套接流
+        }catch (\Exception $e){
+            throw new KafkaException(['code'=>71,'message'=>$e->getMessage()]);
+        }
+    }
+
+    public function __destruct()
+    {
+        socket_close($this->socket);//工作完毕，关闭套接流
+    }
+
+    private $socket;
+}
